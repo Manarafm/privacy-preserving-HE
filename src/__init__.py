47510ef67a380ebf808dee8c6eec58fb2f6faebf
Manarafm <139872996+Manarafm@users.noreply.github.com>
2026-04-16 09:56:27 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,7 @@
 # Copyright (c) 2024 Manar Alnasser / Cardiff University.
 # SPDX-License-Identifier: Apache-2.0
 # Part of the FHE-PHML PhD Research Framework.
+"""
+FHE-PHML: A Privacy-Preserving Heart and Medicine Learning Framework
+using Fully Homomorphic Encryption.
+"""
